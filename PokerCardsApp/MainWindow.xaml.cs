25fc309467fd6dc737af792b17a831a82883dd47
Owen <Floul1@hotmail.co.uk>
2017-11-26 19:11:20 +0000

    Added BlindCreator

--- PokerCardsApp/MainWindow.xaml.cs
+++ PokerCardsApp/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PokerCardsApp.Cards;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using PokerCardsApp.Cards;
+using PokerCardsApp.Poker;
 
 namespace PokerCardsApp
 {
@@ -24,8 +12,7 @@ namespace PokerCardsApp
         public MainWindow()
         {
             InitializeComponent();
-            var test = new CardsWhiteboard();
-            test.Show();
+            new BlindCreator().Show();
         }
     }
 }
---
+++ PokerCardsApp/Poker/BlindCreator.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using NewCardsFramework.Poker;
+using ProtoBuf;
+
+namespace PokerCardsApp.Poker
+{
+    /// <summary>
+    /// Interaction logic for BlindCreator.xaml
+    /// </summary>
+    public partial class BlindCreator : Window
+    {
+        public BlindCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var tourney = new PokerTournamentStructure
+            {
+                BlindTimes = new TimeSpan(0, 20, 0),
+                RebuyAllowed = false,
+                StructureName = "Test Structure"
+            };
+            var levels = new List<BlindLevel>
+            {
+                new BlindLevel(50,100,0),
+                new BlindLevel(75,150,0),
+                new BlindLevel(100,200,0),
+                new BlindLevel(100,200,25),
+                new BlindLevel(150,300,25),
+                new BlindLevel(200,400,50),
+                new BlindLevel(250,500,50),
+                new BlindLevel(300,600,75),
+                new BlindLevel(400,800,100),
+                new BlindLevel(500,1000,100)
+            };
+            tourney.BlindLevels = levels;
+            var file = File.Open(@"C:\memes\test.tournament", FileMode.OpenOrCreate);
+            Serializer.Serialize(file, tourney);
+            file.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var file = File.Open(@"C:\memes\test.tournament", FileMode.Open))
+            {
+                var results = Serializer.Deserialize<PokerTournamentStructure>(file);
+            }
+        }
+    }
+}
